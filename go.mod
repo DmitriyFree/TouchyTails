5eb46a2c081e8ff0919c5f41d6f59f465a429963
Dmitriy Andreev <dmitriy.free@icloud.com>
2025-08-21 10:42:58 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module TouchyTails
+
+go 1.24.6
+
+require github.com/hypebeast/go-osc v0.0.0-20220308234300-cec5a8a1e5f5 // indirect
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hypebeast/go-osc/osc"
+)
+
+func main() {
+	// Create a dispatcher (router for OSC addresses)
+	dispatcher := osc.NewStandardDispatcher()
+
+	// Add handler for our avatar parameter
+	dispatcher.AddMsgHandler("/avatar/parameters/TouchedChest", func(msg *osc.Message) {
+		if len(msg.Arguments) > 0 {
+			if val, ok := msg.Arguments[0].(float32); ok {
+				if val > 0.5 {
+					fmt.Println("Touch detected! Trigger haptics ON")
+				} else {
+					fmt.Println("Touch released! Haptics OFF")
+				}
+			}
+		}
+	})
+
+	// Start server
+	server := &osc.Server{
+		Addr:       "127.0.0.1:9000",
+		Dispatcher: dispatcher,
+	}
+
+	log.Printf("Listening for OSC on %s...\n", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
