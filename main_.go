88a2b3b8c37334b13de0ba15e5b4e1ab81cb7493
June <113283077+DmitriyFree@users.noreply.github.com>
2025-08-22 22:13:54 +0700

    tailtouching works

--- main_.go
+++ main_.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -12,22 +11,20 @@ func main() {
 	// Create a dispatcher (router for OSC addresses)
 	dispatcher := osc.NewStandardDispatcher()
 
-	// Add handler for our avatar parameter
-	dispatcher.AddMsgHandler("/avatar/parameters/TouchedTail", func(msg *osc.Message) {
-		if len(msg.Arguments) > 0 {
-			if val, ok := msg.Arguments[0].(float32); ok {
-				if val > 0.5 {
-					fmt.Println("Touch detected! Trigger haptics ON")
-				} else {
-					fmt.Println("Touch released! Haptics OFF")
-				}
-			}
-		}
+	// Add handler for our avatar parameter ///avatar/parameters/TouchedTail
+	/*dispatcher.AddMsgHandler("*", func(msg *osc.Message) {
+		fmt.Printf("Received OSC message: %s\n", msg.Address)
+		//fmt.Printf("Arguments: %v\n", msg.Arguments)
+	})*/
+	// Add handler for our avatar parameter //
+	dispatcher.AddMsgHandler("/avatar/parameters/TailTouch", func(msg *osc.Message) {
+		fmt.Printf("Received OSC message: %s\n", msg.Address)
+		fmt.Printf("Arguments: %v\n", msg.Arguments)
 	})
 
 	// Start server
 	server := &osc.Server{
-		Addr:       "127.0.0.1:9000",
+		Addr:       "127.0.0.1:9001",
 		Dispatcher: dispatcher,
 	}
 
